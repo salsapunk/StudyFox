14f83270523d59719f6acb7374d789b3e07e9f16
lucarl07 <07lucarl.dev@gmail.com>
2026-06-05 17:53:12 -0300

    🚧 feat(temp): Criar diretório "temp_data" para guardar dados mockup

---
+++ src/temp_data/tarefas.js
@@ -0,0 +1,50 @@
+export default [
+	{
+		id: 1,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 1,
+		codigo: 0
+	},
+	{
+		id: 2,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 2,
+		codigo: 0
+	},
+	{
+		id: 3,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 3,
+		codigo: 0
+	},
+	{
+		id: 4,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 1,
+		codigo: 0
+	},
+	{
+		id: 5,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 3,
+		codigo: 0
+	},
+	{
+		id: 6,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 3,
+		codigo: 0
+	}
+]
