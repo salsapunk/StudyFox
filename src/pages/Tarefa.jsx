5bdabab56a80a4e94be83c1d1b035cc2d38a4ac6
lucarl07 <07lucarl.dev@gmail.com>
2026-06-06 19:05:55 -0300

    feat(pages): Estruturar a página Tarefa

--- src/pages/Tarefa.jsx
+++ src/pages/Tarefa.jsx
@@ -1,6 +1,31 @@
+import { useLoaderData } from "react-router"
+import $ from '@/styles/Tarefa.module.css'
+
 export default function Tarefa() {
-  return (
-    <div>Tarefa</div>
-  )
+	const { nome, prazo, anotacao } = useLoaderData();
+
+	return (
+		<section className={$.modal}>
+			<header>
+				<hgroup>
+					<h2>{nome}</h2>
+					{/*<p>{prazo}</p>*/}
+				</hgroup>
+				<div className={$.button_wrapper}>
+					<button>
+						<img src="#" alt="Apagar tarefa"/>
+					</button>
+					<button>
+						<img src="#" alt="Fechar janela"/>
+					</button>
+				</div>	
+			</header>
+			<article>
+				<p className={$.task_annotation}>
+					{anotacao}
+				</p>
+			</article>
+		</section>
+	) 
 }
 
