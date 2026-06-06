933a416f8ccb13be32e04941c186e74ad55d5130
lucarl07 <07lucarl.dev@gmail.com>
2026-06-06 16:42:35 -0300

    feat(temp_data): Adicionar tarefas de matérias distintas

--- src/temp_data/tarefas.js
+++ src/temp_data/tarefas.js
@@ -5,7 +5,7 @@ export default [
 		prazo: new Date('2026-06-05T17:23:00'),
 		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		coluna: 1,
-		codigo: 0
+		codigo: 1
 	},
 	{
 		id: 2,
@@ -13,7 +13,7 @@ export default [
 		prazo: new Date('2026-06-05T17:23:00'),
 		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		coluna: 2,
-		codigo: 0
+		codigo: 1
 	},
 	{
 		id: 3,
@@ -21,7 +21,7 @@ export default [
 		prazo: new Date('2026-06-05T17:23:00'),
 		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		coluna: 3,
-		codigo: 0
+		codigo: 1
 	},
 	{
 		id: 4,
@@ -29,7 +29,7 @@ export default [
 		prazo: new Date('2026-06-05T17:23:00'),
 		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		coluna: 1,
-		codigo: 0
+		codigo: 1
 	},
 	{
 		id: 5,
@@ -37,7 +37,7 @@ export default [
 		prazo: new Date('2026-06-05T17:23:00'),
 		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		coluna: 3,
-		codigo: 0
+		codigo: 1
 	},
 	{
 		id: 6,
@@ -45,6 +45,54 @@ export default [
 		prazo: new Date('2026-06-05T17:23:00'),
 		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		coluna: 3,
-		codigo: 0
+		codigo: 1
+	},
+	{
+		id: 7,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 1,
+		codigo: 2
+	},
+	{
+		id: 8,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 2,
+		codigo: 2
+	},
+	{
+		id: 9,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 3,
+		codigo: 2
+	},
+	{
+		id: 10,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 1,
+		codigo: 2
+	},
+	{
+		id: 11,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 1,
+		codigo: 2
+	},
+	{
+		id: 12,
+		nome: 'Lorem ipsum dolor sit amet',
+		prazo: new Date('2026-06-05T17:23:00'),
+		anotacao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		coluna: 2,
+		codigo: 2
 	}
 ]
