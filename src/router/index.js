e7b744a4aeaa68d9e9d4c4b90d3aaecbb3c1bbcd
lucarl07 <07lucarl.dev@gmail.com>
2026-06-07 18:45:49 -0300

    chore(router): Mover router para o diretório @/router

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,22 @@
 import { createBrowserRouter } from "react-router";
 
 // Páginas:
-import Inicio from "./pages/Inicio";
-import Login from "./pages/Login";
-import Cadastro from "./pages/Cadastro";
-import Materias from "./pages/Materias";
-import Opcoes from "./pages/Opcoes.jsx";
-import Tarefas from "./pages/Tarefas";
-import Tarefa from "./pages/Tarefa";
-import CriarTarefa from "./pages/CriarTarefa";
+import Inicio from "@/pages/Inicio";
+import Login from "@/pages/Login";
+import Cadastro from "@/pages/Cadastro";
+import Materias from "@/pages/Materias";
+import Opcoes from "@/pages/Opcoes.jsx";
+import Tarefas from "@/pages/Tarefas";
+import Tarefa from "@/pages/Tarefa";
+import CriarTarefa from "@/pages/CriarTarefa";
 
 // Layouts:
-import MainLayout from "./layouts/MainLayout";
+import MainLayout from "@/layouts/MainLayout";
 
 // Loaders e Actions:
-import tarefaLoader from "./api/tarefaLoader";
-import tarefasLoader from "./api/tarefasLoader";
+import tarefaLoader from "@/api/tarefaLoader";
+import tarefasLoader from "@/api/tarefasLoader";
+import authLoader from "@/api/authLoader";
 
 let router = createBrowserRouter([
 	{
@@ -27,6 +28,7 @@ let router = createBrowserRouter([
 			{ path: "/opcoes", Component: Opcoes },
 			{
 				Component: MainLayout,
+				// loader: authLoader,
 				children: [
 					{ path: "/materias", Component: Materias },
 					{
