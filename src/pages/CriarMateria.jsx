c2c781d1639bfcf60bfead656bfc53345330da4f
lucarl07 <07lucarl.dev@gmail.com>
2026-06-07 23:39:08 -0300

    refactor(pages): Estruturar CriarMateria adequadamente para sua função

--- src/pages/CriarMateria.jsx
+++ src/pages/CriarMateria.jsx
@@ -1,56 +1,33 @@
-import { useReducer } from "react"
-import { useNavigate } from 'react-router'
-
+import { Form, useActionData, useNavigate } from 'react-router'
+import CloseWindow from '@/assets/icons/close-window.svg?react'
 import $ from '@/styles/CriarMateria.module.css'
 
-import CloseWindow from '@/assets/icons/close-window.svg?react'
-
 export default function CriarMateria() {
-	const initialFormState = {
-		nome: ''
-	}
+  const actionData = useActionData()
+  const navigate = useNavigate()
 
-	const navigate = useNavigate()
-	const [formState, alterFormState] = useReducer((prev, next) => {
-		return { ...prev, ...next }
-	}, initialFormState)
-
-	const handleExitModal = () => navigate(-1)
-	const onFormSubmit = (event) => {
-		event.preventDefault()
-		/* Lógica da API aqui... */
-		navigate(-1)
-	}
-
-	return (
-		<form className={$.modal} onSubmit={onFormSubmit}>
-			<header className={$.form_header}>
-				<h2>Nova matéria</h2>
-				<button 
-					className={$.btn_exit}
-					type="button"
-					onClick={handleExitModal}
-				>
-					<CloseWindow className={$.svg} />
-				</button>
-			</header>
-			<div className={$.form_content}>
-				<div className={$.input_wrapper}>
-					<label htmlFor="nome">Nome:</label>
-					<input 
-						type="text" name="nome" 
-						value={formState.nome}
-						onChange={(e) => alterFormState({ nome: e.target.value })}
-					/>
-				</div>
-				<button 
-					className={$.btn_submit}
-					type="submit"
-				>
-					Criar matéria
-				</button>
-			</div>
-		</form>
-	)
+  return (
+    <Form className={$.modal} method="post">
+      <header className={$.form_header}>
+        <h2>Nova matéria</h2>
+        <button
+          className={$.btn_exit}
+          type="button"
+          onClick={() => navigate(-1)}
+        >
+          <CloseWindow className={$.svg} />
+        </button>
+      </header>
+      <div className={$.form_content}>
+        <div className={$.input_wrapper}>
+          <label htmlFor="nome">Nome:</label>
+          <input type="text" name="nome" />
+        </div>
+        {actionData?.erro && <p>{actionData.erro}</p>}
+        <button className={$.btn_submit} type="submit">
+          Criar matéria
+        </button>
+      </div>
+    </Form>
+  )
 }
-
