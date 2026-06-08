79bd82cfa33c0dd9e373da7ef105f8b1fad06c5a
lucarl07 <07lucarl.dev@gmail.com>
2026-06-07 23:42:35 -0300

    style(pages): Indentação em CriarMateria

--- src/pages/CriarMateria.jsx
+++ src/pages/CriarMateria.jsx
@@ -3,31 +3,31 @@ import CloseWindow from '@/assets/icons/close-window.svg?react'
 import $ from '@/styles/CriarMateria.module.css'
 
 export default function CriarMateria() {
-  const actionData = useActionData()
-  const navigate = useNavigate()
+	const actionData = useActionData()
+	const navigate = useNavigate()
 
-  return (
-    <Form className={$.modal} method="post">
-      <header className={$.form_header}>
-        <h2>Nova matéria</h2>
-        <button
-          className={$.btn_exit}
-          type="button"
-          onClick={() => navigate(-1)}
-        >
-          <CloseWindow className={$.svg} />
-        </button>
-      </header>
-      <div className={$.form_content}>
-        <div className={$.input_wrapper}>
-          <label htmlFor="nome">Nome:</label>
-          <input type="text" name="nome" />
-        </div>
-        {actionData?.erro && <p>{actionData.erro}</p>}
-        <button className={$.btn_submit} type="submit">
-          Criar matéria
-        </button>
-      </div>
-    </Form>
-  )
+	return (
+		<Form className={$.modal} method="post">
+			<header className={$.form_header}>
+				<h2>Nova matéria</h2>
+				<button
+					className={$.btn_exit}
+					type="button"
+					onClick={() => navigate(-1)}
+				>
+					<CloseWindow className={$.svg} />
+				</button>
+			</header>
+			<div className={$.form_content}>
+				<div className={$.input_wrapper}>
+					<label htmlFor="nome">Nome:</label>
+					<input type="text" name="nome" />
+				</div>
+				{actionData?.erro && <p>{actionData.erro}</p>}
+				<button className={$.btn_submit} type="submit">
+					Criar matéria
+				</button>
+			</div>
+		</Form>
+	)
 }
