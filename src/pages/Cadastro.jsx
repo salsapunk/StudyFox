33cbc7aecf190758287136848b003ea7309e3d8c
N3rdMusical05 <jp3188619@gmail.com>
2026-06-08 08:16:34 -0300

    fix: css

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -7,44 +7,44 @@ import studyfox from "../assets/images/logotype.png";
 
 export default function Cadastro() { 
   return (
-    <div className={styles.PáginaCadastro}>
+    <div className="PáginaCadastro">
 
-      <div className={styles.ParteEsquerda}>
-        <img className={styles.MensagemBoasVindas} src={bemvindo} alt="Mensagem de boas vindas"/>
-        <img className={styles.RaposaAcenando} src={raposaacenando} alt="Raposa fofinha"/>
+      <div className="ParteEsquerda">
+        <img className="MensagemBoasVindas" src={bemvindo} alt="Mensagem de boas vindas"/>
+        <img className="RaposaAcenando" src={raposaacenando} alt="Raposa fofinha"/>
       </div>
 
-      <div className={styles.ParteDireita}>
-        <img className={styles.NomeProjeto} src={studyfox} alt="Este é nome do projeto"/>
+      <div className="ParteDireita">
+        <img className="NomeProjeto" src={studyfox} alt="Este é nome do projeto"/>
         <p>Create your acount:</p>
       </div>
 
-      <div className={styles.Formulário}>
-        <div className={styles.CriaçãoEmail}>
+      <div className="Formulário">
+        <div className="CriaçãoEmail">
             <label htmlFor="email">Email:</label>
             <input type="email" id="email" name="Email"/>
         </div>
 
-        <div className={styles.CriaçãoSenha}>
+        <div className="CriaçãoSenha">
             <label htmlFor="senha">Password:</label>
             <input type="password" id="password" name="Password"/>
         </div>
 
-        <button className={styles.BotãoCadastrar} onClick={() => console.log("Criando usuário...")}>
+        <button className="BotãoCadastrar" onClick={() => console.log("Criando usuário...")}>
           Sign in
         </button>
 
-        <div className={styles.OutrosMeios}>
-          <button className={styles.BotãoOutrosMeios} aria-label="Login com Google"/>
-          <button className={styles.BotãoOutrosMeios} aria-label="Login com FaceBook"/>
-          <button className={styles.BotãoOutrosMeios} aria-label="Login com Instagram"/>
+        <div className="OutrosMeios">
+          <button className="BotãoOutrosMeios" aria-label="Login com Google"/>
+          <button className="BotãoOutrosMeios" aria-label="Login com FaceBook"/>
+          <button className="BotãoOutrosMeios" aria-label="Login com Instagram"/>
         </div>
 
-        <div className={styles.PossuoConta}>
+        <div className="PossuoConta">
           <p>Alredy have an acount?</p>
 
-          <Link to="/login">
-          <button className={styles.LoginMinhaConta}>
+          <Link to="Login">
+          <button className="LoginMinhaConta">
             Login
           </button>
           </Link>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,19 +32,19 @@ export default function Login() {
     }
 
     return (
-        <div className={styles.PáginaLogin}>
+        <div className="PáginaLogin">
 
 
 
-            <form className={styles.ParteEsquerda} onSubmit={onAttemptLogin}>
+            <form className="ParteEsquerda" onSubmit={onAttemptLogin}>
 
-              <div className={styles.login_header}>
-                <img src={studyfox} alt="Logo StudyFox" className={styles.LogoStudyfox} />
-                <p className={styles.login_subtitle}>Login to your account:</p>
+              <div className="login-header">
+                <img src={studyfox} alt="Logo StudyFox" className="LogoStudyfox" />
+                <p className="login-subtitle">Login to your account:</p>
               </div>
 
-                <div className={styles.Formulário}>
-                    <div className={styles.LoginEmail}>
+                <div className="Formulário">
+                    <div className="LoginEmail">
                         <label htmlFor="email">Email:</label>
                         <input
                             type="email"
@@ -56,7 +56,7 @@ export default function Login() {
                         />
                     </div>
 
-                    <div className={styles.LoginSenha}>
+                    <div className="LoginSenha">
                         <label htmlFor="senha">Password:</label>
                         <input
                             type="password"
@@ -69,23 +69,23 @@ export default function Login() {
                     </div>
                 </div>
 
-                {erro && <p className={styles.LoginErro} role="alert">{erro}</p>}
+                {erro && <p className="LoginErro" role="alert">{erro}</p>}
 
-                <button className={styles.BotãoLogin} type="submit" disabled={carregando}>
+                <button className="BotãoLogin" type="submit" disabled={carregando}>
                     {carregando ? "Entrando..." : "Login"}
                 </button>
 
-                <div className={styles.OutrosMeios}>
-                    <button className={styles.BotãoOutrosMeios} aria-label="Login com Google" />
-                    <button className={styles.BotãoOutrosMeios} aria-label="Login com Facebook" />
-                    <button className={styles.BotãoOutrosMeios} aria-label="Login com Instagram" />
+                <div className="OutrosMeios">
+                    <button className="BotãoOutrosMeios" aria-label="Login com Google" />
+                    <button className="BotãoOutrosMeios" aria-label="Login com Facebook" />
+                    <button className="BotãoOutrosMeios" aria-label="Login com Instagram" />
                 </div>
 
-				<div className={styles.SignUp}>
+				<div className="SignUp">
 				  <p>Don't have an account?</p>
 
 				  <Link to="/Cadastro">
-				  <button className={styles.Cadastrar} onClick={() => console.log("Aguarde...")}>
+				  <button className="Cadastrar" onClick={() => console.log("Aguarde...")}>
 					Sign up
 				  </button>
 				  </Link>
@@ -93,9 +93,9 @@ export default function Login() {
 				</div>
       </form>
 
-      <div className={styles.ParteDireita}>
-        <img src={bemvindo} alt="Bem-vindo de volta!" className={styles.bemvindodevolta} />
-        <img src={raposasentada} alt="Raposa fofa" className={styles.RaposaSentada} />
+      <div className="ParteDireita">
+        <img src={bemvindo} alt="Bem-vindo de volta!" className="bemvindodevolta" />
+        <img src={raposasentada} alt="Raposa fofa" className="RaposaSentada" />
       </div>
 
     </div>
